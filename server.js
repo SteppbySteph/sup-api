343fce6167008469d489854c84e795b2ee236b75
Josefin Eliasson <91781497+Josse79@users.noreply.github.com>
2022-05-20 13:21:11 +0200

    Api started, datafile and endpoints added

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 
+import listEndpoints from "express-list-endpoints";
+import destinations from "./data/destinations.json"
+
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
@@ -13,8 +16,38 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send(listEndpoints(app));
 });
+
+app.get("/destinations", (req, res) => {
+
+  res.status(200).json({
+    data: destinations,
+    success: true,
+  });
+});
+
+app.get("/destinations/destination/:category", (req, res) => {
+
+  const { category } = req.params
+
+   const destinationByCategory = destinations.filter((destination) =>
+   destination.category.toLowerCase() === category);
+
+  if (destinationByCategory) {
+    res.status(200).json({
+      data: destinationByCategory,
+      success: true,
+    });
+  } else {
+    res.status(404).json({
+      data: "Destination not found.",
+      success: false,
+    });
+  }
+});
+
+
 
 // Start the server
 app.listen(port, () => {
