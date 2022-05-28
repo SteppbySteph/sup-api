ff9bca8c4a4c70fe608f7267b77601e26debeb22
Josefin Eliasson <91781497+Josse79@users.noreply.github.com>
2022-05-28 12:44:27 +0200

    added endpoints for user to register, update and delete

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express"
 import cors from "cors"
 import mongoose from "mongoose"
+import bcrypt from "bcrypt"
+import crypto from "crypto"
 
 import listEndpoints from "express-list-endpoints"
 import destinations from "./data/destinations.json"
@@ -58,7 +60,6 @@ const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     minlength: 8,
-    maxlength: 30,
     required: true,
     unique: true
   },
@@ -66,11 +67,13 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
     match: /.+\@.+\..+/,
-    unique: true
+    unique: true,
+    trim: true
   },
   password: {
     type: String,
-    required: true
+    required: true,
+    trim: true
   },
   accessToken: {
     type: String,
@@ -78,6 +81,155 @@ const UserSchema = new mongoose.Schema({
   }
 })
 
+const User = mongoose.model("User", UserSchema)
+
+//create a user
+app.post ("/register", async (req, res) => {
+
+  const { username, email, password } = req.body;
+  try {
+    const salt = bcrypt.genSaltSync()
+
+    if(username.length < 8) {
+      res.status(400).json ({
+        response: "Username must be at least 8 characters long",
+        success: false
+      })
+    } else {
+      const newUser = await new User({
+        username: username,
+        email: email,
+        password: bcrypt.hashSync(password, salt)
+      }).save()
+      res.status(201).json({
+        response: {
+          username: newUser.username,
+          email: newUser.email,
+          accessToken: newUser.accessToken,
+          userId: newUser._id
+        },
+        success: true
+      })
+    }
+  } catch(error) {
+    res.status(400).json({
+      response: "Something went wrong. Please check your credentials.",
+      success: false
+    })
+   }
+  })
+
+//delete a user  
+app.delete("/users/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try { 
+    const deleted = await User.findOneAndDelete({_id: id});
+    if (deleted) {
+      res.status(200).json({
+        success: true, 
+        response: `User ${deleted.username} has been deleted.`
+      });
+    } else {
+      res.status(404).json({
+        success: false, response: "Not found"
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      success: false, 
+      response: error
+    });
+  }
+});
+
+//update user
+app.patch("/users/:id", async (req, res) => {
+  const { id } = req.params
+  const { updatedName } = req.body
+
+  try {
+    const userToUpdate = await User.findByIdAndUpdate({_id: id}, {username: updatedName})
+    if (userToUpdate) {
+      res.status(200).json({success: true, response: `User ${userToUpdate.username} has been updated`})
+    } else {
+      res.status(404).json({success: false, response: "Not found"})
+    }
+  } catch (error) {
+    res.status(400).json({success: false, response: error})
+  }
+})
+
+//endpoint for user login
+app.post("/login", async (req, res) => {
+  const { username, password, email } = req.body;
+
+  try {
+    const user = await User.findOne({username, email});
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        success: true,
+        username: user.username,
+        email: user.email,
+        accessToken: user.accessToken,
+        userId: user._id
+      })
+    } else {
+      res.status(400).json({
+        response: "Credentials don't match",
+        success: false
+      }) 
+    }
+  } catch (error) {
+    res.status(400).json({
+      response: error,
+      success: false
+    })
+  }
+})
+
+//use below to enter post section
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header("Authorization")
+  try {
+    const user = await User.findOne({accessToken: accessToken})
+    if (user) {
+      next();
+    } else {
+      res.status(401).json({
+        response: "Please log in",
+        success: false
+      })
+    }
+  } catch (error) {
+    res.status(400).json({
+      response: error,
+      success: false
+    });
+  }
+}
+
+
+
+
+
+  // const emailPattern = /.+\@.+\..+/;
+      
+  //   if (email !== passwordTwo) {
+  //     setError("Passwords do not match.");
+  //   } else if (password.match(passwordPattern) && username.length > 4) {
+  //     registerUser({
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify({ username, password }),
+  //     });
+  //   } else {
+  //     setError(
+  //       "Password needs to be between 8 and 30 characters and contain at least one uppercase letter, one lowercase letter, one special symbol, and one number."
+  //     );
+  //   }
+  // };
 
 
 // Start the server
